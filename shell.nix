a12048cec28ac57c919f818174cc46f3e45b2625
Adam Matthews <adamdmatthews@users.noreply.github.com>
2023-12-21 20:19:30 +0000

    added a nix-shell file for playwright tests

---
+++ shell.nix
@@ -0,0 +1,6 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [ playwright-driver.browsers ];
+    shellHook = "export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}";
+}
+
