ecd4320961786eb037fcfed61d6be9f2a453798a
Adam Matthews <adamdmatthews@users.noreply.github.com>
2023-12-21 19:02:07 +0000

    replaced demo app with a page that shows a clock

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,17 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+// I'd like a cleaner way to handle the current time in tests
+// but I guess this is good enough for now, flaky tests can just be rerun
+
+test.beforeEach(async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+})
+
+test('page has correct title', async ({ page }) => {
+	await expect(page).toHaveTitle('remainder');
+});
+
+test('clock gets current time', async ({ page }) => {
+	const date = new Date();
+	await expect(page.getByRole('heading')).toHaveText(date.toLocaleTimeString('en-GB'));
 });
