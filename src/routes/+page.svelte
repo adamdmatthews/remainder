e04d3184f35704834d823276d988842e5ed005c2
Adam Matthews <adamdmatthews@users.noreply.github.com>
2023-12-21 19:02:07 +0000

    replaced demo app with a page that shows a clock

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { format } from 'date-fns/format';
+
+    let date = new Date();
+    $: formatted_time = format(date, 'HH:mm:ss');
+
+    onMount(() => {
+        const interval = setInterval(() => {
+            date = new Date();
+        }, 100);
+        return () => clearInterval(interval);
+    })
+</script>
+
+<h1>{formatted_time}</h1>
