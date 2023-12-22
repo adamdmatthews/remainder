ad7d78a817b8110161c5f22217d6fc374067f450
Adam Matthews <adamdmatthews@users.noreply.github.com>
2023-12-22 14:22:08 +0000

    added a content-security-policy

--- tests/observatory_test.ts
+++ tests/observatory_test.ts
@@ -5,3 +5,10 @@ test("has meta description", async ({ page }) => {
     const metaDescription = page.locator('meta[name="description"]');
     expect(await metaDescription.getAttribute("content")).toBe("Keep track of deadlines");
 })
+
+test("has security policy header", async ({ page }) => {
+    const response = await page.goto("/");
+    const headers = response!.headers()!;
+    expect(headers["content-security-policy"]).toContain("default-src 'none'");
+})
+    
